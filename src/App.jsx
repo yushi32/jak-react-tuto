479a3bf5c6b17a2abfea4e2b78b2846c556585e5
yushi32 <133078588+yushi32@users.noreply.github.com>
2023-09-28 16:18:17 +0000

    learn: ReactでのCSSの適用

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 
+
 const App = () => {
   const onClickButton = () => alert();
+  const contentStyle = {
+    color: 'blue',
+    fontSize: '18px'
+  };
   return (
     <>
-      <h1>こんにちは！</h1>
-      <p>お元気ですか？</p>
-      <button onClick = {onClickButton}>ボタン</button>
+      <h1 style={{ color: 'red' }}>こんにちは！</h1>
+      <p style={contentStyle}>お元気ですか？</p>
+      <button onClick={onClickButton}>ボタン</button>
     </>
   );
 };
